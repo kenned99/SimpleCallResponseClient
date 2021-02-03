9672179feed72d65050174dd77165efac07a2dcc
Kenneth Jensen <kenneth_jensen_99@hotmail.com>
2021-02-03 10:12:49 +0100

    first commit

---
+++ SimpleCallResponse/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCallResponse
+{
+    class Encryption
+    {
+        public Encryption()
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes("a");
+            byte key = 255;
+            PrintBytesUTF8(bytes);
+            EncryptByte(bytes, key);
+            PrintBytesUTF8(bytes);
+            DecryptByte(bytes, key);
+            PrintBytesUTF8(bytes);
+        }
+
+        public void EncryptByte(byte[] bytes, byte key)
+        {
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                int change = i + key;
+                while (change + bytes[i] > 255) change -= 256;
+                bytes[i] += (byte)change;
+            }
+        }
+
+        public void DecryptByte(byte[] bytes, byte key)
+        {
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                int change = i + key;
+                while (change - bytes[i] < 0) change += 256;
+                bytes[i] -= (byte)change;
+            }
+        }
+
+        public void PrintBytesUTF8(byte[] bytes)
+        {
+            string toPrint = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(toPrint);
+        }
+    }
+}
---
+++ SimpleCallResponse/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleCallResponse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            int port = 13356;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+            Console.WriteLine("Hvad vil du?\n\"c\" for at annullere");
+            RecieveMessage(stream);
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() == "c")
+                {
+                    break;
+                }
+
+                byte[] buffer = Encoding.UTF8.GetBytes(userInput);
+                stream.Write(buffer, 0, buffer.Length);
+
+            }
+            client.Close();
+        }
+
+        public static async void RecieveMessage(NetworkStream stream)
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[256];
+                int nBytesRead = await stream.ReadAsync(buffer, 0, 256);
+                string message = Encoding.UTF8.GetString(buffer, 0, nBytesRead);
+                if (message != string.Empty)
+                {
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
